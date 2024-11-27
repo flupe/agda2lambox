cd3e43d0d143aaa9b5a8f445014d31dcd807aeb9
Orestis Melkonian <melkon.or@gmail.com>
2024-11-27 11:55:31 +0100

    Bump GHC version + rename

---
+++ src/LambdaBox.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE LambdaCase #-}
+module LambdaBox where
+
+import Data.Maybe ( fromMaybe )
+import Control.Monad ( unless )
+import Control.Monad.IO.Class ( MonadIO(liftIO) )
+import Control.DeepSeq ( NFData(..) )
+
+import System.Console.GetOpt ( OptDescr(Option), ArgDescr(ReqArg) )
+
+import Data.Version ( showVersion )
+import Paths_agda2min ( version )
+
+import Agda.Syntax.Common.Pretty ( prettyShow )
+import Agda.Syntax.Internal ( qnameName, qnameModule )
+import Agda.Syntax.TopLevelModuleName ( TopLevelModuleName, moduleNameToFileName )
+
+import Agda.Compiler.Common ( curIF, compileDir )
+import Agda.Compiler.Backend ( Backend(..), Backend'(..), Recompile(..), IsMain )
+
+import Agda.TypeChecking.Monad.Base ( Definition(..) )
+import Agda.TypeChecking.Monad
+  ( TCM, withCurrentModule, iInsideScope, setScope
+  , CompilerPragma(..), getUniqueCompilerPragma )
+
+data ETerm
+  =
+  |
+  |
