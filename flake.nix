26c855f38f519f1ac742b0e6b27df069cb5ff189
Bohdan <17168103+liesnikov@users.noreply.github.com>
2024-11-27 17:03:14 +0100

    nix stuff

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Agda2lambox";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = {self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {inherit system;};
+        agda2lambox = pkgs.haskellPackages.callCabal2nix "agda2lambox" ./. {};
+      in {
+        packages = {
+          inherit agda2lambox;
+          default = agda2lambox;
+        };
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = p: [agda2lambox];
+          buildInputs = with pkgs.haskellPackages; [
+            cabal-install
+            haskell-language-server
+          ];
+        };
+      });
+}
