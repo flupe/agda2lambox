bef0d1dd6fff3d87b6ee3ee989ea5d11191399d2
flupe <lucas@escot.me>
2025-01-10 19:35:28 +0100

    prove decidability of kername freshness

--- CheckWF.v
+++ CheckWF.v
@@ -14,22 +14,29 @@ Import EnvMap.
 Definition inspect {A} (a : A) : {b | a = b} := exist _ a eq_refl.
 Notation "x 'eqn:' p" := (exist _ x p) (only parsing, at level 20).
 
-(* TODO: Freshness of kername in the environment is decidable *)
-Definition dec_fresh_global
-  (k     : kername)
-  (decls : global_declarations)
-  : decidable (fresh_global k decls).
-Admitted.
+Print Forall.
+Print fresh_global.
 
 #[local] Obligation Tactic :=
           try intro wf_decls;
           try dependent elimination wf_decls;
           try auto.
 
-Equations? dec_wf_glob {efl : EEnvFlags} (decls : global_declarations)
-  : decidable (wf_glob decls) :=
+Equations? dec_fresh_global (k : kername) (decls : global_declarations) : decidable (fresh_global k decls) :=
+dec_fresh_global k [] := or_introl (Forall_nil _);
+dec_fresh_global k ((k', _)::ds) with dec_fresh_global k ds := {
+  | or_intror rotten_k_ds := or_intror _;
+  | or_introl fresh_k_ds  with eq_dec k' k := {
+     | left  k'eqk  := or_intror _;
+     | right k'neqk := or_introl _
+  };
+}.
+- apply Forall_cons; auto.
+Defined.
+
+Equations? dec_wf_glob {efl : EEnvFlags} (decls : global_declarations) : decidable (wf_glob decls) :=
 dec_wf_glob [] := or_introl wf_glob_nil;
-dec_wf_glob ((k,d)::ds) with dec_wf_glob ds := {
+dec_wf_glob ((k, d)::ds) with dec_wf_glob ds := {
   | or_introl wf_ds with inspect (wf_global_decl ds d), dec_fresh_global k ds := {
       | true  eqn:     wf_d | or_introl fresh_k  := or_introl _;
       | true  eqn:     wf_d | or_intror rotten_k := or_intror _;
