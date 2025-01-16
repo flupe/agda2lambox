c1d3e1fddbf741a20c897b6ed00d9654197b477e
flupe <lucas@escot.me>
2025-01-16 12:53:51 +0100

    start cleaning up

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,13 @@
-{-# LANGUAGE DeriveGeneric, DeriveAnyClass, OverloadedStrings #-}
-
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+-- | The agda2lambox Agda backend
 module Main where
 
 import Control.Monad ( unless )
-import Control.Monad.IO.Class ( MonadIO(liftIO) )
-import Control.DeepSeq ( NFData(..) )
+import Control.Monad.IO.Class ( liftIO )
+import Control.DeepSeq ( NFData )
+import Data.Function ( (&) )
 import Data.IORef ( IORef, newIORef, readIORef, modifyIORef' )
-import Data.Maybe ( fromMaybe, catMaybes, isJust, isNothing )
+import Data.Maybe ( fromMaybe, catMaybes )
 import Data.Version ( showVersion )
 import GHC.Generics ( Generic )
 import System.Console.GetOpt ( OptDescr(Option), ArgDescr(ReqArg) )
@@ -15,70 +16,79 @@ import System.FilePath ( (</>) )
 
 import Paths_agda2lambox ( version )
 
-import Agda.Lib hiding ( (<?>), pretty )
-import Agda.Syntax.Common.Pretty ( (<?>), pretty )
-import Agda.Utils ( pp, unqual, hasPragma )
+import Agda.Lib
+import Agda.Utils ( pp, hasPragma )
 
 import Agda2Lambox.Convert ( convert )
 import Agda2Lambox.Convert.Function ( convertFunction )
 import Agda2Lambox.Convert.Data     ( convertDatatype )
 import Agda2Lambox.Convert.Record   ( convertRecord   )
 import Agda2Lambox.Monad ( runC0, inMutuals )
-import CoqGen ( ToCoq(ToCoq) )
-import LambdaBox ( KerName, GlobalDecl, qnameToKerName, CoqModule(CoqModule) )
+import CoqGen    ( ToCoq(ToCoq) )
+import LambdaBox ( KerName, GlobalDecl, qnameToKerName, CoqModule(..) )
 
 
 main :: IO ()
-main = runAgda [Backend backend]
+main = runAgda [agda2lambox]
 
--- | LambdaBox backend options.
+
+-- | Backend options.
 data Options = Options { optOutDir :: Maybe FilePath }
   deriving (Generic, NFData)
 
--- | Setter for backend output directory option.
+-- | Setter for output directory option.
 outdirOpt :: Monad m => FilePath -> Options -> m Options
 outdirOpt dir opts = return opts { optOutDir = Just dir }
 
+-- | Default backend options.
 defaultOptions :: Options
 defaultOptions = Options { optOutDir = Nothing }
 
-
+-- | Backend module environments.
 data ModuleEnv = ModuleEnv 
   { modProgs :: IORef [KerName]
-     -- ^ Names of programs to evaluate in a module
+     -- ^ Names of programs to extract in a module
   }
 
 type ModuleRes = ()
 
-backend :: Backend' Options Options ModuleEnv ModuleRes (Maybe (KerName, GlobalDecl))
-backend = Backend'
-  { backendName           = "agda2lambox"
-  , backendVersion        = Just (showVersion version)
-  , options               = defaultOptions
-  , commandLineFlags      =
-      [ Option ['o'] ["out-dir"] (ReqArg outdirOpt "DIR")
-        "Write output files to DIR. (default: project root)"
-      ]
-  , isEnabled             = \ _ -> True
-  , preCompile            = return
-  , postCompile           = \ _ _ _ -> return ()
-  , preModule             = moduleSetup
-  , postModule            = writeModule
-  , compileDef            = compile
-  , scopeCheckingSuffices = False
-  , mayEraseType          = \ _ -> return True
-  }
+-- | The adga2lambox backend.
+agda2lambox :: Backend
+agda2lambox = Backend backend
+  where
+    backend :: Backend' Options Options ModuleEnv ModuleRes (Maybe (KerName, GlobalDecl))
+    backend = Backend'
+      { backendName           = "agda2lambox"
+      , backendVersion        = Just $ showVersion version
+      , options               = defaultOptions
+      , commandLineFlags      =
+          [ Option ['o'] ["out-dir"] (ReqArg outdirOpt "DIR")
+            "Write output files to DIR. (default: project root)"
+          ]
+      , isEnabled             = \ _ -> True
+      , preCompile            = return
+      , postCompile           = \ _ _ _ -> return ()
+      , preModule             = moduleSetup
+      , postModule            = writeModule
+      , compileDef            = compileDefinition
+      , scopeCheckingSuffices = False
+      , mayEraseType          = \ _ -> return True
+      }
 
 
-moduleSetup :: Options -> IsMain -> TopLevelModuleName -> Maybe FilePath
-            -> TCM (Recompile ModuleEnv ModuleRes)
+moduleSetup
+  :: Options -> IsMain -> TopLevelModuleName -> Maybe FilePath
+  -> TCM (Recompile ModuleEnv ModuleRes)
 moduleSetup _ _ m _ = do
   setScope . iInsideScope =<< curIF
   Recompile . ModuleEnv <$> liftIO (newIORef [])
 
 
-compile :: Options -> ModuleEnv -> IsMain -> Definition -> TCM (Maybe (KerName, GlobalDecl))
-compile opts menv _ def@Defn{..} =
+compileDefinition
+  :: Options -> ModuleEnv -> IsMain
+  -> Definition
+  -> TCM (Maybe (KerName, GlobalDecl))
+compileDefinition opts menv _ def@Defn{..} =
   fmap (qnameToKerName defName,) <$> -- prepend kername
     case theDef of
 
@@ -97,27 +107,26 @@ compile opts menv _ def@Defn{..} =
       _          -> Nothing <$ (liftIO $ putStrLn $ "Skipping " <> prettyShow defName)
 
 
-writeModule :: Options -> ModuleEnv -> IsMain -> TopLevelModuleName
-            -> [Maybe (KerName, GlobalDecl)]
-            -> TCM ModuleRes
+-- | Collect global definitions and programs in a module
+-- and write them to disk.
+writeModule
+  :: Options -> ModuleEnv -> IsMain -> TopLevelModuleName
+  -> [Maybe (KerName, GlobalDecl)]
+  -> TCM ModuleRes
 writeModule opts menv _ m (reverse . catMaybes -> cdefs) = do
-  progs   <- liftIO $ readIORef $ modProgs menv
-  compDir <- compileDir
-
-  let outDir   = fromMaybe compDir (optOutDir opts)
-      fileName = (outDir </>) . moduleNameToFileName m
+  programs   <- liftIO $ readIORef $ modProgs menv
+  outDir     <- flip fromMaybe (optOutDir opts) <$> compileDir
 
   liftIO $ createDirectoryIfMissing True outDir
 
-  let mod = CoqModule cdefs progs
+  let fileName = (outDir </>) . moduleNameToFileName m
+      coqMod   = CoqModule cdefs programs
 
   unless (null cdefs) $ liftIO do
     putStrLn $ "Writing " <> fileName ".{v,txt}"
 
-    writeFile (fileName ".txt")
-      $ (<> "\n")
-      $ pp $ mod
+    pp coqMod <> "\n"
+      & writeFile (fileName ".txt")
 
-    writeFile (fileName ".v")
-      $ (<> "\n")
-      $ pp $ ToCoq $ mod
+    pp (ToCoq coqMod) <> "\n"
+      & writeFile (fileName ".v")
